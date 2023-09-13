384731d86d16e0fc12783ce4eeb3a923be62bb40
NL <nila0001@stud.kea.dk>
2023-09-13 15:27:26 +0200

    v4

--- produkter.js
+++ produkter.js
@@ -1,28 +1,14 @@
-document.querySelector(".cardarrow").addEventListener("click", flip);
-
-function flip() {
-  document.querySelector(".produktcard").classList.remove("cardarrowfliptilbage");
-  document.querySelector(".produktcard").classList.add("cardarrowflip");
-  document.querySelector(".cardarrow").removeEventListener("click", flip);
-  document.querySelector(".cardarrowbagside").addEventListener("click", flipBagside);
-}
-
-function flipBagside() {
-  document.querySelector(".produktcard").classList.add("cardarrowfliptilbage");
-  document.querySelector(".produktcard").classList.remove("cardarrowflip");
-  document.querySelector(".cardarrow").addEventListener("click", flip);
-  document.querySelector(".cardarrowbagside").removeEventListener("click", flip);
-}
-
 // Vælger alle items med en bestemt kategori med "select" https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?select=id,navn,kategori
 // Vælger items i en bestemt kategori ""=eq"  https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?kategori=eq.Svampe
 // De kan kombineret kateogir=eq.Svampe&select=navn,id
 
 const urlParams = new URLSearchParams(window.location.search);
-const kategori = urlParams.get("kateogir");
+//Landskab
+const kategori = urlParams.get("kategori");
+//Svampe, Urter, Buske, Tang
 const produkt = urlParams.get("produkt");
 
-fetch(`https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?select=navn&landskab=eq.${kategori}&kategori=eq.${produkt}`, {
+fetch(`https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?select=navn,landskab,landskab2,landskab_sub1,landskab_sub2,image&kategori=eq.${produkt}&find=eq.${kategori}`, {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkeWt4bnNkdGtxeW51aXh0YWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1NzcwMTUsImV4cCI6MjAwOTE1MzAxNX0.fjEwMgmSK8h0wye_IlwHu2uE21eB2hofndsJH0IKa3E",
@@ -31,6 +17,27 @@ fetch(`https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?select=navn&landska
   .then((res) => res.json())
   .then(showData);
 
-function showData(kats) {
-  console.log(kats);
+function showData(produkter) {
+  produkter.forEach(showProduct);
+}
+
+function showProduct(product) {
+  const template = document.querySelector("template").content;
+
+  const copy = template.cloneNode(true);
+  copy.querySelector(".cardbillede img").src = product.image;
+  copy.querySelector("h2").textContent = product.navn;
+  copy.querySelector(".h2").textContent = product.navn;
+  copy.querySelector(".findmig").textContent = product.landskab;
+  copy.querySelector(".findmig2").textContent = product.landskab2;
+
+  document.querySelector(".card").appendChild(copy);
+
+  const arrowr = document.querySelectorAll(".produktcard");
+
+  arrowr.forEach((arrowr) => arrowr.addEventListener("click", flip));
+
+  function flip() {
+    this.classList.toggle("cardarrowflip");
+  }
 }
