f80116f68693748c5d12a4364d699bfbda68d9c7
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-09-13 07:44:27 +0200

    kommentarer til js

--- kategori.js
+++ kategori.js
@@ -1,24 +1,34 @@
+// Denne linje bruger Fetch API til at sende en GET-anmodning til den angivne URL.
+// Den henter data fra en ekstern kilde (https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?subkategori).
 fetch("https://xdykxnsdtkqynuixtakq.supabase.co/rest/v1/data?subkategori", {
   method: "GET",
   headers: {
+    // API-nøglen sendes som en del af headeren for at godkende anmodningen.
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkeWt4bnNkdGtxeW51aXh0YWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1NzcwMTUsImV4cCI6MjAwOTE1MzAxNX0.fjEwMgmSK8h0wye_IlwHu2uE21eB2hofndsJH0IKa3E",
   },
 })
-    .then(res => res.json())
-    .then(showCategories)
+  // Den følgende linje konverterer svaret til JSON-format.
+  .then(res => res.json())
+  // Når JSON-dataen er klar, kaldes funktionen "showCategories" med dataen som argument.
+  .then(showCategories);
 
-    function showCategories(cats){
-        console.log(cats);
-        cats.forEach(showCategory)
-    }
+// Denne funktion viser kategorierne i konsollen og kalder funktionen "showCategory" for hver kategori.
+function showCategories(cats){
+    console.log(cats);
+    cats.forEach(showCategory)
+}
 
-    function showCategory(cat){
-        const template = document.querySelector("template").content;
+// Denne funktion tager en kategori som argument og opretter et klon af en HTML-skabelon.
+function showCategory(cat){
+    const template = document.querySelector("template").content;
 
-        const clone = template.cloneNode(true);
+    const clone = template.cloneNode(true);
 
-        clone.querySelector("a").textContent=cat.category;
-        clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
+    // Sætter tekstindholdet af linket til kategorinavnet.
+    clone.querySelector("a").textContent=cat.category;
+    // Sætter href-attributten på linket til en URL med kategoriens navn.
+    clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
 
-        document.querySelector("#categories").appendChild(clone);
-    }
+    // Tilføjer den klonede kategori til DOM'en.
+    document.querySelector("#categories").appendChild(clone);
+}
